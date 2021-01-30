0066abe4cf08afadc161d1900ac6775c2da5dd87
bianfusia <44915719+bianfusia@users.noreply.github.com>
2021-01-30 14:32:27 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -40,12 +40,13 @@ def get_word(start_letter,con_letter,no_word):
     man_list = list(filter(lambda x: (con_letter in x), man_list))
 
     full_int = len(man_list)-1
-    rand_word = (random.randint(0,full_int))
-    chosen_word = man_list[rand_word]
-    keep_list.remove(chosen_word)
 
     if full_int < 0:
         return print("\n" + "no available words detected" + "\n")
+
+    rand_word = (random.randint(0,full_int))
+    chosen_word = man_list[rand_word]
+    keep_list.remove(chosen_word)
 
     print("\n" + man_list.pop(rand_word) + "\n")
         
@@ -59,6 +60,7 @@ def get_last(start_letter, last_letter):
     man_list = list(filter(lambda x: (last_letter == x[-1]), man_list))
     man_list = list(filter(lambda x: (len(x) >= 10), man_list))
     man_list = list(filter(lambda x : (x[0] == start_letter), man_list))
+    man_list = list(filter(lambda x: (con_letter in x), man_list))
 
     full_int = len(man_list)-1
 
@@ -66,6 +68,36 @@ def get_last(start_letter, last_letter):
         man_list = keep_list.copy()
         man_list = list(filter(lambda x : (x[0] == start_letter), man_list))
         man_list = list(filter(lambda x: (len(x) >= 10), man_list))
+        man_list = list(filter(lambda x: (con_letter in x), man_list))
+        full_int = len(man_list)-1
+        rand_word = (random.randint(0,full_int))
+        chosen_word = man_list[rand_word]
+        keep_list.remove(chosen_word)
+        print("\n" + man_list.pop(rand_word)+ "\n")
+    
+    else:
+
+        full_int = len(man_list)-1
+        rand_word = (random.randint(0,full_int))
+        chosen_word = man_list[rand_word]
+        keep_list.remove(chosen_word)
+        print("\n" + man_list.pop(rand_word) + "\n")
+
+def ded_last(start_letter, last_letter, con_letter, no_word):
+
+    global keep_list
+    man_list = keep_list.copy()
+    
+    man_list = list(filter(lambda x: (last_letter == x[-1]), man_list))
+    man_list = list(filter(lambda x: (len(x) >= no_word), man_list))
+    man_list = list(filter(lambda x : (x[0] == start_letter), man_list))
+
+    full_int = len(man_list)-1
+
+    if full_int < 0:
+        man_list = keep_list.copy()
+        man_list = list(filter(lambda x : (x[0] == start_letter), man_list))
+        man_list = list(filter(lambda x: (len(x) >= no_word), man_list))
         full_int = len(man_list)-1
         rand_word = (random.randint(0,full_int))
         chosen_word = man_list[rand_word]
@@ -91,7 +123,7 @@ while True:
 
     keep_list = word_list.copy()
 
-    choice = int(input("\n" + "1: required letter\n2: hard mode\nPlease choose a mode:"))
+    choice = int(input("\n" + "1: required letter\n2: hard mode\n3: deathtorl\nPlease choose a mode:"))
 
     while True:
         if choice == 1:
@@ -120,5 +152,19 @@ while True:
                 if start_letter == "1":
                     break
                 get_last(start_letter, last_letter)
+
+        elif choice == 3:
+
+            print("\n" + "Key 1 at the start letter to end game")
+            last_letter = str(input("How friendly do you want to be?: "))
+
+            while True:
+
+                start_letter = str(input("\n" + "key in the start letter: "))
+                con_letter = str(input("\n" + "key in the containing letter: "))
+                no_word = int(input("number of words: "))
+                if start_letter == "1":
+                    break
+                ded_last(start_letter, last_letter,con_letter,no_word)
 
             break
